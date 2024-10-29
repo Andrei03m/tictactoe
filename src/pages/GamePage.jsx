027a30fefb027c625a0f3357c0327a313101205f
Andrei <andrei.constantin03m@gmail.com>
2024-10-29 21:59:15 +0200

    Create main game page

---
+++ src/pages/GamePage.jsx
@@ -0,0 +1,14 @@
+const GamePage = () => {
+  return (
+    <div className="game-container">
+      <div className="player">
+        <h2>Player X</h2>
+      </div>
+      <div className="player">
+        <h2>Player 0</h2>
+      </div>
+    </div>
+  );
+};
+
+export default GamePage;
